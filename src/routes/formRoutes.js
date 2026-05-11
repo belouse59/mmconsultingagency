1a730c8bd81a09e1620a3af3c9ab0af83bad0c9f
Belouse <clement.leuridon>
2026-05-11 21:11:59 +0200

    debug formRoutes

--- src/routes/formRoutes.js
+++ src/routes/formRoutes.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const router = express.Router();
 const { submitForm } = require("../controllers/formController");
-
+console.log("submitForm in formRoutes");
 router.post("/submit", submitForm);
 
 module.exports = router;
